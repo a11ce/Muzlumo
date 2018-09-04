2a3b13748365d2d9eb6b66cee76defe4bf9a4ca1
a11ce <a11ce@protonmail.com>
2018-09-03 22:12:30 -0400

    Color based on volume
    The hue is arbitrary

--- muzlumo.py
+++ muzlumo.py
@@ -1,5 +1,10 @@
 import pyaudio
 import audioop
+import pygame
+import colorsys
+
+def map( x,  in_min,  in_max,  out_min,  out_max):
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 
 def main():
     inpFormat = pyaudio.paInt16
@@ -7,7 +12,12 @@ def main():
     inpRate = 44100
     inpBlockSize = 1024
 
+    pygame.init()
+    screen = pygame.display.set_mode((200,200))
     audio = pyaudio.PyAudio()
+    clock = pygame.time.Clock()
+    pygame.display.update()
+
 
     inpStream = audio.open( format=inpFormat,
                             channels=inpChannels,
@@ -15,10 +25,17 @@ def main():
                             input=True)
 
     while True:
+        pygame.event.pump()
+
         data = inpStream.read(inpBlockSize)
         volume = audioop.rms(data, 2)
-        print(volume)
 
-
+        volMapDec = map(volume,0,9000,0,100)/100.0
+        color = colorsys.hsv_to_rgb(0.9, 1, volMapDec)
+        color = tuple(int(round(map(i,0.0,1.0,0.0,255.0 )))for i in color)
+        print(color)
+        screen.fill(color)
+        pygame.display.update()
+        clock.tick(60)
 if __name__ == '__main__':
     main()
