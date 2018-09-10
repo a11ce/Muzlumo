08ebe061e55f398f5373569e4f4cd555cac39bd4
a11ce <a11ce@protonmail.com>
2018-09-09 20:20:15 -0400

    Pitch influences hue
    Works on most songs, kind of choppy at low volumes

--- muzlumo.py
+++ muzlumo.py
@@ -25,27 +25,52 @@ def main():
     clock = pygame.time.Clock()
     pygame.display.update()
 
+    pDetection = aubio.pitch("default", 2048,2048//2, 44100)
+    pDetection.set_unit("midi")
+    pDetection.set_silence(-40)
 
     inpStream = audio.open( format=inpFormat,
                             channels=inpChannels,
                             rate=inpRate,
                             input=True)
     maxVol = 0
+    minPitch = 48
+    maxPitch = 48
     while True:
         for e in pygame.event.get():
             if ((e.type == pygame.KEYDOWN) and (e.key == pygame.K_q)):
                 sys.exit()
 
-        data = inpStream.read(inpBlockSize)
+        try:
+            data = inpStream.read(inpBlockSize)
+        except Exception as e:
+            pass
+
         aubioData = np.fromstring(data, dtype=aubio.float_type)
 
         volume = np.sum(aubioData**2)/len(aubioData)
-        print(volume)
-        if (volume == 0): maxVol = 0
+        if (volume == 0): maxVol = 0.00000000000000001
         if (volume>maxVol): maxVol = volume
         volMapDec = map(volume,0,maxVol,0,100)/100.0
-        color = colorsys.hsv_to_rgb(0.9, 1, volMapDec)
+
+        pitch = pDetection(aubioData)[0]
+        if (pitch>maxPitch and pitch<5000): maxPitch = pitch
+        if (pitch<minPitch and pitch > 0): minPitch = pitch
+        if (pitch==0):
+            minPitch = 48
+            maxPitch = 48
+            pitch = (minPitch+maxPitch)/2
+
+
+        try:
+            pitchMapDec = map(pitch, minPitch, maxPitch, 0, 87.5)/100.0
+            int(pitchMapDec)
+        except Exception as e:
+            pitchMapDec = 0.5
+        color = colorsys.hsv_to_rgb(pitchMapDec, 1, volMapDec)
+
         color = tuple(int(round(map(i,0.0,1.0,0.0,255.0 )))for i in color)
+
         print(color)
         screen.fill(color)
         pygame.display.update()
